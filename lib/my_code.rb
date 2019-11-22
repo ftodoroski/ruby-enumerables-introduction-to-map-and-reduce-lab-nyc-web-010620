cf796342af996764f9df166a09949f510c7a8b58
Filip Todoroski <ftodoroski@ymail.com>
2019-11-22 23:41:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,18 @@ def map_to_negativize(array)
 
   new_array
 end
+
+def  map_to_no_change(array)
+  new_array = []
+
+  i = 0
+  while i < array.length
+    element = array[i]
+
+    new_array << element
+
+    i += 1
+  end
+
+  new_array
+end

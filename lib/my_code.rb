826045f39e41465e36e5722a678b5ad286c8418a
Filip Todoroski <ftodoroski@ymail.com>
2019-11-22 23:42:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,18 @@ def  map_to_no_change(array)
 
   new_array
 end
+
+def   map_to_double(array)
+  new_array = []
+
+  i = 0
+  while i < array.length
+    element = array[i]
+
+    new_array << (element * 2)
+
+    i += 1
+  end
+
+  new_array
+end
